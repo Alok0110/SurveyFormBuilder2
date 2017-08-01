669d4404ce815c653448a3378d48b01e03380a25
Alok Dethe <dethealok@gmail.com>
2017-08-01 17:46:45 +1200

    new changes

--- resources/js/jofferSurveyBuilder.js
+++ resources/js/jofferSurveyBuilder.js
@@ -1,95 +1,42 @@
-function User(firstname,lastname) {
+/* 
+ *
+ * Copyright (C) Joffer Systems, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Alok Dethe <det.alex0110@gmail.com>, July 2017
+ *
+ *
+ *
+ */
+
+;(function(global, $){
     
-    if( typeof User.instance === 'object' ) {
-        console.log("inside the if block ==> ");
-        return User.instance;
+    /*
+     * Check if jquery is loaded first  
+     */
+    if( !$ ){
+        throw new  Error("jQuery not loaded");
     }
     
-    
-    
-    this.firstname = firstname;
-        //"john";
-    this.lastname = lastname;
-        //"doe";
-    
-    console.log("outside the if block ==> "+this.firstname+" "+this.lastname);
-    
-    User.instance = this;
-    
-    //return this;
-    
-}
-
-User.remo = "ok";
-
-console.log(" show function object var => "+User.remo);
-
-User("john","doe");
-User("john1","doe1");
-var v1 = new User("john1","doe1");
-console.log("==> fname "+v1.firstname+"==> lname "+v1.lastname);
-console.log( v1.prototype );
-
-
-function Hem() {
-    var fname="john", lname="doe";
-}
-
-var v2 = new Hem();
-console.log("v2 prototype");
-
-var v3 = {
-    fi: "john",
-    la: "doe"
-}
-
-console.log( v2.prototype );
-console.log("v3 prototype");
-console.log( v3.prototype );
-
-/*
-*Singleton with closure
-*/
-function UserTwo() {
-    var instance;
-    
-    UserTwo.prototype = this;
-    instance = new UserTwo();
-    
-    
-    
-    UserTwo = function() {
-        return instance;
+    /*
+     *Default variables
+     */
+    var JofferSurveyFormBuilderInit,
+     
+    /*
+     *Default Settings
+     */
+    defaults = {
+        
+        
     };
     
-    instance.constructor = UserTwo;
-    
-    
-    
-    instance.firstname = "john";
-    instance.lastname = "doe";
-    
-    return instance;
-}
-
-/*
-*Singleton in IIFE
-*/
-var UserFour;
-(function(){
-    var instance;
-    
-    UserFour = function UserFour() {
-        if( instance ) {
-            return instance;
-        }
-        
-        instance = this;
-        
-        this.firstname = "john";
-        this.lastname = "doe";
-        
-        return instance;
+    /*
+     *Initialize the library
+     */
+    $.JofferSurveyFormBuilderInit = function() {
+           
     };
     
-}());
+    
+}(window, jQuery));
---
+++ resources/js/myscript.js
@@ -0,0 +1,2 @@
+$.JofferSurveyFormBuilderInit();
+$.fn.parTTT();
