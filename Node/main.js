855e3ba9e58ff67a63ae76347aa8f2e1568fef23
Alok Dethe <dethealok@gmail.com>
2017-08-03 20:52:59 +1200

    node js files

---
+++ Node/main.js
@@ -0,0 +1,16 @@
+/*
+ * NodeJS
+ */
+
+var http = require("http");
+
+http.createServer(function(request,response){
+    
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    response.end('Hello World\n');
+    
+}).listen(8989);
+
+
+console.log("this is inside main.js file");
