04c518e3567c683dff83e8b5243ba69f3ce9f1a6
Alok Dethe <dethealok@gmail.com>
2017-08-20 17:32:10 +1200

    drag and drop

--- resources/js/jofferSurveyBuilder.js
+++ resources/js/jofferSurveyBuilder.js
@@ -24,18 +24,71 @@
     /*
      *Default Settings
      */
-    const defaults = {
-        setone: "fir"
+    var defaults = {
+        recentElement: ''
     };
     
     /*
      *Initialize the library
      */
     $.JofferSurveyFormBuilderInit = function() {
-           var testVar = 10;
-           console.log("test it ==>"+this.testVar+" "+defaults.setone);
-           
-           
+        
+        var self = this;
+        
+        $( "#header-id, #number-id" ).draggable({  
+           /* connectToSortable: "#ele-container-id", */
+            helper: "clone", 
+            cursorAt: { top: 4, left: 10 }, 
+            containment: "#big-cont", scroll: false, 
+            start: function() {
+                defaults.recentElement = $( this ).attr("id");
+                
+             },
+            stop: function() {
+                
+                console.log( "==>"+$( this ).attr("id")+" ok "+$("#drag-here-id").hasClass("hide-ele") );
+            },                      
+            revert: "invalid"
+       });
+
+        self.attachToBody = function( ele ) {
+                        defaults.recentElement = '';
+                        if ( !$("#drag-here-id").hasClass("hide-ele") ) {
+                                $("#drag-here-id").addClass("hide-ele");
+                        }
+                        var cl = $( ele ).clone();
+                        cl.appendTo( ".ele-container-cl" );
+                        cl.addClass("show-ele");
+                        cl.removeClass("head-li-cl");
+                        console.log("ok got in header-id");
+        }
+        
+        $( "#droppable" ).droppable({
+              accept: "#header-id, #number-id",
+              drop: function( event, ui ) {
+                switch( defaults.recentElement ){
+                        
+                    case "header-id" : 
+                        self.attachToBody(".head-li-cl");
+                        break;
+                        
+                    case "number-id" :
+                        self.attachToBody(".number-li-cl");
+                        break;
+                        
+                    default :
+                        break;
+                        
+                }
+            
+              }
+        });
+        
+        $( "#ele-container-id" ).sortable({
+          revert: true
+        });
+        
+        
     };
     
     
